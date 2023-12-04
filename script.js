615e72b0230c982f962c4f6d01b8c5fcff4eb800
mannyAddisu <codesbycodex@gmail.com>
2023-12-04 15:17:21 +0300

    updated session tasks

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
 //Online session
 //functions
-function factorial(num) {
-  let fact = 1;
-  for (let i = num; i >= 1; i--) {
-    fact *= i;
+'use strict';
+// function factorial(num) {
+//   let fact = 1;
+//   for (let i = num; i >= 1; i--) {
+//     fact *= i;
+//   }
+//   return fact;
+// }
+// // console.log(factorial(5));
+/******today's task******/
+// function printOdd() {
+//   const end = prompt('Enter a number: ');
+//   for (let i = 0; i <= end; i++) {
+//     if (i % 2 !== 0) console.log(i);
+//   }
+// }
+// printOdd();
+
+//Session 3
+/*********Counter *********/
+const increase = document.getElementById('increase');
+const decrease = document.getElementById('decrease');
+
+let click = true;
+let count = 0;
+increase.addEventListener('click', function () {
+  if (click) {
+    count += 1;
+    document.getElementById('count').innerHTML = count;
   }
-  return fact;
-}
-// console.log(factorial(5));
-//today's task
-function printOdd() {
-  const end = prompt("Enter a number: ");
-  for (let i = 0; i <= end; i++) {
-    if (i % 2 !== 0) console.log(i + " ");
+});
+decrease.addEventListener('click', function () {
+  if (click) {
+    count -= 1;
+    document.getElementById('count').innerHTML = count;
   }
-}
-printOdd();
+});
+let num1, num2, result;
